d7351c93da56b77237013d60d15efbdfa6dda767
Elliott Arnold <46078321+si3mshady@users.noreply.github.com>
2024-02-09 03:51:41 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <aside className='side-menu'>
+      <div className='side-menu-button'>
+        <span>+</span>
+        
+        
+        NEW CHAT</div>
+     </aside>
+     <section className='chatbox'>
+
+     </section>
+    </div>
+  );
+}
+
+export default App;
