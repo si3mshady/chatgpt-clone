94bc0007395c51e79b847641e92150d545481eec
Elliott Arnold <si3mshady@Elliotts-MBP.lan>
2024-02-22 07:52:16 -0600

    save point

--- src/terraform /main.tf
+++ src/terraform /main.tf
@@ -7,7 +7,7 @@ resource "google_compute_instance" "sandbox0" {
     device_name = "sandbox0"
 
     initialize_params {
-      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20240213"
+      image = "projects/debian-cloud/global/images/debian-10-buster-v20240213"
       size  = 100
       type  = "pd-balanced"
     }
@@ -17,14 +17,19 @@ resource "google_compute_instance" "sandbox0" {
 
   can_ip_forward      = false
   deletion_protection = false
-  enable_display      = true
+  enable_display      = false
 
   labels = {
     goog-ec-src = "vm_add-tf"
   }
 
-  machine_type = "e2-standard-2"
-  name         = "sandbox0"
+  machine_type = "e2-medium"
+
+  metadata = {
+    ssh-keys = "el.cloud.science.lab:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDCqdbvAw3APqIdJOo0XO547PUGigMbnPjPOp9m+RL7J2h1KtHl+PEYWyscXicIhxIOOGKte1jj7wUzy9u+NTKd3BDhuY3oaR1/CMA8KaE0pNDbinUSvz4UMG17OZRpeVX7TslT0bMnzBl02xMAOGcbgYpFxFl2p9ki29pKkiI3vmfni6o6I85cjPfkswIExRYOWjdW0wJzWlY0tqdrd25dC6jvxVOePr75G9MS63sj6H7qAb/1pH6cxGOveeiR2kBE4Rl1jOifuvDUd8idZdpVUS2UMs4RIinoN2S0QFcgC/48ZCpbYuP1Bb/O5jIwV4DHYSEiVX4lPB3c4pRqx11AIO68BwsIKGTpHLqrw5Z6f4jl8q+b6rEy+XWafOj/N+HCjtSzdhgacRll62Doyk7JFxNFpUzs7sks7O8HRclz60dVR2nHlgA3UAaol2dxzvTpvjkfRlHmcsZjLEteRaICHMEgqRBLlve32Bwa5ayhawnmGRYgqaQGobqERDOljjU= el.cloud.science.lab@gmail.com"
+  }
+
+  name = "sandbox0"
 
   network_interface {
     access_config {
@@ -45,13 +50,13 @@ resource "google_compute_instance" "sandbox0" {
 
   service_account {
     email  = "267242228702-compute@developer.gserviceaccount.com"
-    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
   }
 
   shielded_instance_config {
-    enable_integrity_monitoring = true
+    enable_integrity_monitoring = false
     enable_secure_boot          = false
-    enable_vtpm                 = true
+    enable_vtpm                 = false
   }
 
   tags = ["http-server", "https-server"]
